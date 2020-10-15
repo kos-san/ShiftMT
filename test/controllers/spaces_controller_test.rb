e4f7be9cb6689200891362aeffd79f411e0bd767
kos-san <makocchya917@gmail.com>
2020-10-15 21:52:51 +0900

    新規登録ページの編集

--- test/controllers/spaces_controller_test.rb
+++ test/controllers/spaces_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class SpacesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get spaces_index_url
     assert_response :success
   end
-
 end
