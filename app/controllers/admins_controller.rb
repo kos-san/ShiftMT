d9832efb5e6803b7edabdc748843839b069b9227
kos-san <makocchya917@gmail.com>
2020-10-27 22:31:19 +0900

    修正

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,11 +1,12 @@
 class AdminsController < ApplicationController
+  before_action :set_member
+  before_action :set_admin
 
   def index
   end
 
   def new
-    @member = Member.find(params[:member_id])
-    @user = User.find(@member.user_id)
+    
     @admins = Admin.all
   end
 
@@ -16,6 +17,42 @@ class AdminsController < ApplicationController
     if @admin.valid?
       @admin.save
       redirect_to store_members_path
+    end
+  end
+
+  private
+
+  # 店舗ページのメンバーであればtrueを返すようにする
+  def set_member
+    @member = Member.find(params[:member_id])
+    @user = User.find(@member.user_id)
+    @store = Store.find(params[:store_id])
+    @store.members.each do |member|
+      if member.user_id == @user.id
+        return @current_store_member = true
+      else
+        @current_store_member = false
+      end
+    end
+    if @store.user_id == current_user.id
+      @current_store_member = true
+    end
+  end
+
+  # 店舗ページの管理者であればtrueを返すようにする
+  def set_admin
+    @member = Member.find(params[:member_id])
+    @user = User.find(@member.user_id)
+    @store = Store.find(params[:store_id])
+    @store.admins.each do |admin|
+      if admin.member.user_id == @user.id
+        return @current_store_admin = true
+      else
+        @current_store_admin = false
+      end
+    end
+    if @store.user_id == @user.id
+      @current_store_admin = true
     end
   end
 
