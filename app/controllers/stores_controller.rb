e3fa1e491507720d02a1eb0b56898d090bb9c1dc
kos-san <makocchya917@gmail.com>
2020-10-27 21:59:23 +0900

    修正

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -13,16 +13,21 @@ class StoresController < ApplicationController
   end
 
   def show
-    @store = Store.find(params[:id])
     @stores = Store.all
     @members = Member.all
     @member = Member.where(user_id: current_user.id)
     if current_user.id != @store.user_id
       @admin = Admin.where(member_id: @member[0].id)
     end
-    unless @current_store_member  #メンバーでなければトップページへリダイレクトされる
+    unless @current_store_member 
       redirect_to root_path
     end
+  end
+
+  def destroy
+    store = Store.find(params[:id])
+    store.destroy
+    redirect_to root_path
   end
 
   def create
@@ -39,25 +44,31 @@ class StoresController < ApplicationController
 
   # 店舗ページのメンバーであればtrueを返すようにする
   def set_member
-    store = Store.find(params[:id])
-    store.members.each do |a|
-      if a.user_id == current_user.id || store.user_id == current_user.id #メンバーに店舗テーブルとアソシエーションされたユーザーも加える
+    @store = Store.find(params[:id])
+    @store.members.each do |member|
+      if member.user_id == current_user.id
         return @current_store_member = true
       else
         @current_store_member = false
       end
     end
+    if @store.user_id == current_user.id
+      @current_store_member = true
+    end
   end
 
   # 店舗ページの管理者であればtrueを返すようにする
   def set_admin
-    store = Store.find(params[:id])
-    store.admins.each do |a|
-      if a.member.user_id == current_user.id || store.user_id == current_user.id #店舗テーブルのuser_idは管理者になる
+    @store = Store.find(params[:id])
+    @store.admins.each do |admin|
+      if admin.member.user_id == current_user.id
         return @current_store_admin = true
       else
         @current_store_admin = false
       end
+    end
+    if @store.user_id == current_user.id
+      @current_store_admin = true
     end
   end
 
