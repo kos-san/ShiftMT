83991b943c29e9ea9c46a6196620627e0b56d3e1
kos-san <makocchya917@gmail.com>
2020-10-28 16:34:19 +0900

    バリデーション設定

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,4 +1,10 @@
 class Table < ApplicationRecord
   belongs_to :store
   belongs_to :shift
+
+  with_options presence: true do
+    validates :workday
+    validates :start
+    validates :ending
+  end
 end
