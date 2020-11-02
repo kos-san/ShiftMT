89b679191b518f0becb56354cd15c5374ad69dbd
kos-san <makocchya917@gmail.com>
2020-11-02 18:41:48 +0900

    modelとcontrollerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     end
   end
   resources :stores do
+    resources :contacts
     resources :tables
     resources :shifts
     resources :members do
---
+++ db/migrate/20201102093331_create_contacts.rb
@@ -0,0 +1,11 @@
+class CreateContacts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contacts do |t|
+      t.string     :title, null: false
+      t.text       :text,  null: false
+      t.references :user,               foreign_key: true
+      t.references :store,              foreign_key: true
+      t.timestamps
+    end
+  end
+end
