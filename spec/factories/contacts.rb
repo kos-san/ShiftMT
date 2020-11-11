ce776a689d06b6f38ce4f1bd0de3fde42190ef72
kos-san <makocchya917@gmail.com>
2020-11-11 18:20:42 +0900

    contactモデルテストコードの実装

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :contact do
+    title {Faker::Lorem.sentence}
+    text {Faker::Lorem.sentence}
+
+    association :user
+    association :store
   end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @contact = FactoryBot.build(:contact)
+  end
+
+  context "保存できる場合" do
+    it "全ての情報が揃っていれば登録できる" do
+      expect(@contact).to be_valid
+    end
+
+    it "Titleが空でも登録できる" do
+      @contact.title = nil
+      expect(@contact).to be_valid
+    end
+  end
+
+  context "保存できない場合" do
+    it "textが空では登録できない" do
+      @contact.text = nil
+      @contact.valid?
+      expect(@contact.errors.full_messages).to include("Text can't be blank")
+    end
+
+    it "userが紐付いていなければ登録できない" do
+      @contact.user = nil
+      @contact.valid?
+      expect(@contact.errors.full_messages).to include("User must exist")
+    end
+
+    it "storeが紐付いていなければ登録できない" do
+      @contact.store = nil
+      @contact.valid?
+      expect(@contact.errors.full_messages).to include("Store must exist")
+    end
+
+  end 
 end
